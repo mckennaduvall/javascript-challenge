79a1564d24738b79931c03a331aadbf28d5996dd
Mack D <mckenna.duvall@gmail.com>
2021-06-22 19:11:04 -0600

    Append table data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+console.log(data);
+
+
+data.forEach(function(alienSighting) {
+    console.log(alienSighting);
+    var row = tbody.append("tr");
+
+    Object.entries(alienSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
