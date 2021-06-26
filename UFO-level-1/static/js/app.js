eca772016bf05ca9386d6b6fe062db017c8984c3
Mack D <mckenna.duvall@gmail.com>
2021-06-26 13:52:35 -0600

    Add level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,13 +1,14 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
 
+// reference table body 
 var tbody = d3.select("tbody");
 console.log(data);
 
-
-data.forEach(function(alienSighting) {
+// 
+tableData.forEach(function(alienSighting) {
     console.log(alienSighting);
     var row = tbody.append("tr");
 
@@ -17,3 +18,33 @@ data.forEach(function(alienSighting) {
         cell.text(value);
     });
 });
+
+
+
+//select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function(){
+
+    d3.event.preventDefault();
+    d3.select("tbody").html("");
+
+
+
+//select input date from HTML & get value property
+    var input = d3.select("#datetime");
+    var inputValue = input.property("value");
+
+
+//filter data to equal input value 
+    var filterData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    filterData.forEach(function(chosenDate) {
+        var row = tbody.append("tr");
+        
+        Object.entries(chosenDate).forEach(function([key, value]) {
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+}); 
